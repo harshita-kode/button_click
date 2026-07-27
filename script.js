70977abada5ffe8b7632739aca2ae2ffb4a6df4f
Harshita Kode <harshitakode@gmail.com>
2026-07-27 17:11:02 +0530

    feat: add clear project structure and sound effect

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const link = document.querySelector("#button-link")
 const button = document.querySelector(".button");
 const rect = button.getBoundingClientRect();
 
@@ -5,10 +6,22 @@ const maxX = window.innerWidth - rect.width;
 const maxY = window.innerHeight - rect.height;
 
 button.addEventListener('mouseenter', ()=>{
-
     const top = Math.floor(Math.random()*maxY)+1;
     const left = Math.floor(Math.random()*maxX)+1;
 
     button.style.top = `${top}px`;
     button.style.left = `${left}px`;
+});
+
+button.addEventListener('click', ()=>{
+    let audio = new Audio('./assets/oof.mp3');
+    audio.play();
+});
+
+link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    setTimeout(() => {
+        window.location.href = link.href;
+    }, 1500);
 });
